3231fd1a0a51664bcae111f7f1119fc1557c531f
dannhu283 <dannhu2803@gmail.com>
2023-10-11 22:02:23 +0700

    add calendar movie

--- src/modules/AdminLayout/Movie/CreateShowtimes/index.js
+++ src/modules/AdminLayout/Movie/CreateShowtimes/index.js
@@ -1,1 +1,10 @@
+import styled from "@emotion/styled";
+
 export { default } from "./CreateShowtimes";
+
+export const Title = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  font-style: italic;
+  color: #192a56;
+`;
