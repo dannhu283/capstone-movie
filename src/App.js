768e26b8d7d3f17890bc92b89a1c0ae119ee67d3
dannhu283 <dannhu2803@gmail.com>
2023-10-19 18:43:38 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./modules/Home";
 import Details from "./modules/Details";
 import Signin from "./modules/Auth/pages/Signin";
